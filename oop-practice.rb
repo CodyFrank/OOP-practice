dbcc81a37fa164783cc865578415396c9c2922d8
Cody Frank <cody.frank30@gmail.com>
2020-06-29 10:04:35 -0400

    adds methods to add players to univers

--- oop-practice.rb
+++ oop-practice.rb
@@ -1,17 +1,36 @@
 require "pry"
 
 class Universe
-    def initialize(players = [])
-        @players = players
+
+    attr_accessor :players, :size
+
+    def initialize
+        @players = {}
         @size = 100
+    end
+
+    def add_player(player)
+        raise ArgumentError.new("player must be a player object") if !player.is_a?(Human)
+        taken_locs = @players.values
+        r = Random.new
+        new_loc = r.rand(100)
+        while taken_locs.include?(new_loc)
+            new_loc = r.rand(100)
+        end
+        @players[player] = new_loc
     end
 end
 
 
 class Human 
+
+    attr_accessor :name
+
     def initialize(name)
         @name = name
     end
+
+    
 end
 
 class Warrior < Human
@@ -30,7 +49,7 @@ end
 
 
 
-
+u = Universe.new
 h = Human.new("Cody")
 w = Warrior.new("Arthur")
 wiz = Wizard.new("Merlin")
