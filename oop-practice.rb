b0753bc9774ed2c71f27159b5bfbfc362df4a427
Cody Frank <cody.frank30@gmail.com>
2020-06-29 11:01:35 -0400

    adds walking feature to all humans

--- oop-practice.rb
+++ oop-practice.rb
@@ -4,6 +4,11 @@ class Universe
 
     attr_accessor :players, :size
 
+    def self.common_universe
+        @@common_universe ||= Universe.new
+        return @@common_universe
+    end
+
     def initialize
         @players = {}
         @size = 100
@@ -26,11 +31,43 @@ class Human
 
     attr_accessor :name
 
-    def initialize(name)
-        @name = name
+    def initialize(name, universe = Universe.common_universe)
+        @name, @universe = name, universe
+        @universe.add_player(self)
     end
 
-    
+    def walk(dir)
+        raise ArgumentError.new("dir must be :right of :left") if dir != :right && dir != :left
+
+        if dir == :right
+            if self.loc >= @universe.size - 1
+                puts "you cant go farther that way"
+            else 
+                increment_loc
+            end
+        else
+            if self.loc <= 0 
+                puts "you cant go farther that way"
+            else 
+                decrement_loc
+            end
+        end
+    end
+
+    def loc
+        @universe.players[self]
+    end
+
+    private
+
+    def increment_loc
+        @universe.players[self] += 1
+    end
+
+    def decrement_loc
+        @universe.players[self] -=1
+    end
+
 end
 
 class Warrior < Human
@@ -49,7 +86,7 @@ end
 
 
 
-u = Universe.new
+u = Universe.common_universe
 h = Human.new("Cody")
 w = Warrior.new("Arthur")
 wiz = Wizard.new("Merlin")
